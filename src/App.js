7ba7c85dc599a8156f6cc73952aa3fe9ed5a9d59
oniotna2021 <tonyusb@gmail.com>
2022-02-27 10:49:43 -0500

    reload de la ubicacion por error 2

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
         (data) => setVector(data),
        //console.log(Vector)
       )
-      .catch((err) => alert(err), window.location.reload())
+      .catch((err) => alert(err))
       //Se apago esta condicion porque da error en consola el indice no esta definido
       // .then(({ beats }) => {
       //   beats.forEach((beat, index) => {
